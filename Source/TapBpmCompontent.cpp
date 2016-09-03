4718cbe415a5e38080913796ada60ec6e2b4ddf8
Bao Vuong <vqbao@lv5.org>
2016-09-03 18:41:38 -0400

    initial commit

---
+++ Source/TapBpmCompontent.cpp
@@ -0,0 +1,183 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 4.2.3
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright (c) 2015 - ROLI Ltd.
+
+  ==============================================================================
+*/
+
+//[Headers] You can add your own extra header files here...
+//[/Headers]
+
+#include "TapBpmCompontent.h"
+
+
+//[MiscUserDefs] You can add your own user definitions and misc code here...
+//[/MiscUserDefs]
+
+//==============================================================================
+TapBpmCompontent::TapBpmCompontent ()
+{
+    //[Constructor_pre] You can add your own custom stuff here..
+    //[/Constructor_pre]
+
+    addAndMakeVisible (tapButton = new TextButton ("tap button"));
+    tapButton->setButtonText (TRANS("Tap"));
+    tapButton->addListener (this);
+
+    addAndMakeVisible (bpmTextEditor = new TextEditor ("bpm text editor"));
+    bpmTextEditor->setMultiLine (false);
+    bpmTextEditor->setReturnKeyStartsNewLine (false);
+    bpmTextEditor->setReadOnly (false);
+    bpmTextEditor->setScrollbarsShown (true);
+    bpmTextEditor->setCaretVisible (true);
+    bpmTextEditor->setPopupMenuEnabled (true);
+    bpmTextEditor->setText (String());
+
+    addAndMakeVisible (getBpmButton = new TextButton ("get bpm button"));
+    getBpmButton->setButtonText (TRANS("Get"));
+    getBpmButton->addListener (this);
+
+    addAndMakeVisible (setBpmButton = new TextButton ("set bpm button"));
+    setBpmButton->setButtonText (TRANS("Set"));
+    setBpmButton->addListener (this);
+
+
+    //[UserPreSize]
+    //[/UserPreSize]
+
+    setSize (480, 480);
+
+
+    //[Constructor] You can add your own custom stuff here..
+    bpmTextEditor->setInputRestrictions(3, "0123456789");
+    //[/Constructor]
+}
+
+TapBpmCompontent::~TapBpmCompontent()
+{
+    //[Destructor_pre]. You can add your own custom destruction code here..
+    //[/Destructor_pre]
+
+    tapButton = nullptr;
+    bpmTextEditor = nullptr;
+    getBpmButton = nullptr;
+    setBpmButton = nullptr;
+
+
+    //[Destructor]. You can add your own custom destruction code here..
+    //[/Destructor]
+}
+
+//==============================================================================
+void TapBpmCompontent::paint (Graphics& g)
+{
+    //[UserPrePaint] Add your own custom painting code here..
+    //[/UserPrePaint]
+
+    g.fillAll (Colours::white);
+
+    g.setColour (Colour (0xff3e3f3e));
+    g.fillRoundedRectangle (0.0f, 0.0f, 480.0f, 480.0f, 10.000f);
+
+    g.setColour (Colour (0xff8dd634));
+    g.drawRoundedRectangle (0.0f, 0.0f, 480.0f, 480.0f, 10.000f, 5.000f);
+
+    //[UserPaint] Add your own custom painting code here..
+    //[/UserPaint]
+}
+
+void TapBpmCompontent::resized()
+{
+    //[UserPreResize] Add your own custom resize code here..
+    //[/UserPreResize]
+
+    tapButton->setBounds (8, 80, 464, 392);
+    bpmTextEditor->setBounds (160, 8, 160, 56);
+    getBpmButton->setBounds (8, 8, 150, 56);
+    setBpmButton->setBounds (320, 8, 150, 56);
+    //[UserResized] Add your own custom resize handling here..
+    //[/UserResized]
+}
+
+void TapBpmCompontent::buttonClicked (Button* buttonThatWasClicked)
+{
+    //[UserbuttonClicked_Pre]
+    //[/UserbuttonClicked_Pre]
+
+    if (buttonThatWasClicked == tapButton)
+    {
+        //[UserButtonCode_tapButton] -- add your button handler code here..
+        //[/UserButtonCode_tapButton]
+    }
+    else if (buttonThatWasClicked == getBpmButton)
+    {
+        //[UserButtonCode_getBpmButton] -- add your button handler code here..
+        //[/UserButtonCode_getBpmButton]
+    }
+    else if (buttonThatWasClicked == setBpmButton)
+    {
+        //[UserButtonCode_setBpmButton] -- add your button handler code here..
+        //[/UserButtonCode_setBpmButton]
+    }
+
+    //[UserbuttonClicked_Post]
+    //[/UserbuttonClicked_Post]
+}
+
+
+
+//[MiscUserCode] You can add your own definitions of your custom methods or any other code here...
+//[/MiscUserCode]
+
+
+//==============================================================================
+#if 0
+/*  -- Projucer information section --
+
+    This is where the Projucer stores the metadata that describe this GUI layout, so
+    make changes in here at your peril!
+
+BEGIN_JUCER_METADATA
+
+<JUCER_COMPONENT documentType="Component" className="TapBpmCompontent" componentName=""
+                 parentClasses="public Component" constructorParams="" variableInitialisers=""
+                 snapPixels="8" snapActive="1" snapShown="1" overlayOpacity="0.330"
+                 fixedSize="1" initialWidth="480" initialHeight="480">
+  <BACKGROUND backgroundColour="ffffffff">
+    <ROUNDRECT pos="0 0 480 480" cornerSize="10" fill="solid: ff3e3f3e" hasStroke="1"
+               stroke="5, mitered, butt" strokeColour="solid: ff8dd634"/>
+  </BACKGROUND>
+  <TEXTBUTTON name="tap button" id="77c38d7fdb95c278" memberName="tapButton"
+              virtualName="" explicitFocusOrder="0" pos="8 80 464 392" buttonText="Tap"
+              connectedEdges="0" needsCallback="1" radioGroupId="0"/>
+  <TEXTEDITOR name="bpm text editor" id="8c19f9405ac908c9" memberName="bpmTextEditor"
+              virtualName="" explicitFocusOrder="0" pos="160 8 160 56" initialText=""
+              multiline="0" retKeyStartsLine="0" readonly="0" scrollbars="1"
+              caret="1" popupmenu="1"/>
+  <TEXTBUTTON name="get bpm button" id="53a18679a300d131" memberName="getBpmButton"
+              virtualName="" explicitFocusOrder="0" pos="8 8 150 56" buttonText="Get"
+              connectedEdges="0" needsCallback="1" radioGroupId="0"/>
+  <TEXTBUTTON name="set bpm button" id="fddc3f3871d6b9db" memberName="setBpmButton"
+              virtualName="" explicitFocusOrder="0" pos="320 8 150 56" buttonText="Set"
+              connectedEdges="0" needsCallback="1" radioGroupId="0"/>
+</JUCER_COMPONENT>
+
+END_JUCER_METADATA
+*/
+#endif
+
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
